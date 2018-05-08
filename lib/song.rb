a4a2ed531a2d7af6e90b947ed11b4700a3bde3cf
Christiana Vandermale <christianavdm@gmail.com>
2018-05-08 16:22:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,6 +33,10 @@ class Song
     self.all.detect{|song| song.name == name}
   end
 
-
+  def find_or_create_by_name(name)
+    unless Song.find_by_name(name)
+      Song.create_by_name(name)
+    end
+  end
 
 end
